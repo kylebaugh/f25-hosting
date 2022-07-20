292059ec1a25d0fd42c238be93b22a4406c38ad1
kylebaugh <kylebaugh1@gmail.com>
2022-07-20 12:41:41 -0600

    added js

--- server/index.js
+++ server/index.js
@@ -4,15 +4,15 @@ const path = require('path')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'))
+    res.sendFile(path.join(__dirname, '../frontEnd/index.html'))
 })
 
 app.get('/css', (req, res) =>{
-    res.sendFile(path.join(__dirname, '../styles.css'))
+    res.sendFile(path.join(__dirname, '../frontEnd/styles.css'))
 })
 
 app.get('/js', (req, res) =>{
-    res.sendFile(path.join(__dirname, '../main.js'))
+    res.sendFile(path.join(__dirname, '../frontEnd/main.js'))
 })
 
 const port = process.env.PORT || 4005
